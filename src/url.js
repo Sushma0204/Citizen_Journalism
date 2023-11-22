70e9706edb8c0470eb5e31fec1666252a44c5ac5
Sushma <98072240+Sushma0204@users.noreply.github.com>
2023-11-22 18:41:29 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL=import.meta.env.VITE_URL
-export const IF=import.meta.env.VITE_IF
+export const URL="https://citizenjournalism.onrender.com"
+export const IF="https://citizenjournalism.onrender.com/images/"
